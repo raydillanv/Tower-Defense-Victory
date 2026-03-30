0e50d0423de8374a8eb8dd43aec42a8de1774e88
Dillan Victory <113623616+raydillanv@users.noreply.github.com>
2026-03-30 11:02:10 -0500

    Enemies now follow full map of waypoints, and spawn every second (pre-object pooling)

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private Path currentPath;
     private Vector3 _targetPosition;
+    private int _currentWaypoint;
+
 
     private void Awake()
     {
@@ -16,6 +18,7 @@ public class Enemy : MonoBehaviour
     private void OnEnable()
     {
         // on enable runs before start
+        _currentWaypoint = 0;
         _targetPosition = currentPath.GetPosition(0);
     }
 
@@ -24,6 +27,24 @@ public class Enemy : MonoBehaviour
         
         // moving towards target position
         transform.position = Vector3.MoveTowards(transform.position, _targetPosition, moveSpeed * Time.deltaTime);
+
+        // when the target position is reached we set a new target position
+        float relativeDistance = (transform.position - _targetPosition).magnitude;
+        if (relativeDistance < 0.1f)
+        {
+            // check to make sure its not the last index
+            if (_currentWaypoint < currentPath.Waypoints.Length - 1)
+            {
+                _currentWaypoint++;
+                _targetPosition = currentPath.GetPosition(_currentWaypoint);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+                print(gameObject.name + " reached the end of the map.");
+            }
+
+        }
     }
 
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float _spawnTimer;
+    private float _spawnInterval = 1f;
+
+    public GameObject Prefab;
+
+    void Update()
+    {
+        _spawnTimer -= Time.deltaTime;
+        if (_spawnTimer <= 0)
+        {
+            _spawnTimer = _spawnInterval;
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject spawnedObject = GameObject.Instantiate(Prefab);
+        spawnedObject.transform.position = transform.position;
+    }
+}
